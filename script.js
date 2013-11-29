604a41ed1e854c24a98b7985adcd345927972989
Hector Ramos <hector.ramos@ninthdesign.com>
2013-11-29 08:38:14 +0800

    added input tag

---
+++ script.js
@@ -0,0 +1,35 @@
+var passwordEl = document.getElementById('my-password');
+
+function passwordMeter () {
+    var password = passwordEl.value;
+    var upperCase = /[A-Z]/;
+    var lowerCase = /[a-z]/;
+    var numbers = /[0-9]/;
+    var specialChar = /[!@&)]/;
+    var hasUpperCase = false;
+    var hasLowerCase = false;
+    var hasNumbers = false;
+    var hasSpecialChar = false;
+
+    if (password === '' || password === "undefined") {
+        return false;
+    }
+
+    if (password.search(upperCase) >= 0) {
+        hasUpperCase = true;
+    }
+
+    if (password.search(lowerCase) >= 0) {
+        hasLowerCase = true;
+    }
+
+    if (password.search(numbers) >= 0) {
+        hasNumbers = true;
+    }
+
+    if (password.search(specialChar) >= 0) {
+        hasSpecialChar = true;
+    }
+}
+
+passwordEl.addEventListener("keyup", passwordMeter, false);
